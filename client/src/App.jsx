fb52afb8d8027bdf1f8dca127894f655e0fb8422
Pranav Patkar <pranavpatkar42@gmail.com>
2026-09-19 19:29:01 +0530

    created project builder Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import PreviewPage from './pages/PreviewPage'
 import HomePage from './pages/HomePage'
 import BuilderPage from './pages/BuilderPage'
 import { Toaster } from 'react-hot-toast'
+import PublishPage from './pages/PublishPage'
 
 const App = () => {
   return (
@@ -25,6 +26,9 @@ const App = () => {
         <Route path="/builder/:id" element={<BuilderPage/>} />
         <Route path="/Preview/:id" element={<PreviewPage/>} /> 
       </Route>
+
+      {/* Publish Routes */}
+      <Route path='/publish/:id' element={<PublishPage/>}/>
 
 {/* {catch-all} */}
 <Route path="*" element={<Navigate to="/" replace />} />
